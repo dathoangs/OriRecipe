fc40093c1faf2c0c783b8704510283acda8bb8a7
Hoàng Oanh <hoangoanhhbg@gmail.com>
2023-10-25 09:02:16 +0700

    add activity

---
+++ app/src/main/java/com/example/orirecipe/MyRecipe.java
@@ -0,0 +1,14 @@
+package com.example.orirecipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyRecipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_recipe);
+    }
+}
