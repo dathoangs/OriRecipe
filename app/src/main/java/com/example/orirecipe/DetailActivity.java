bfa54ba85508cfdaf77f6957d8950d33196900b8
Hoàng Oanh <hoangoanhhbg@gmail.com>
2023-10-31 15:40:39 +0700

    add detail Activity

--- app/src/main/java/com/example/orirecipe/DetailActivity.java
+++ app/src/main/java/com/example/orirecipe/DetailActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SigninActivity extends AppCompatActivity {
+public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
+        setContentView(R.layout.activity_detail);
     }
 }
